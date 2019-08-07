c2b8ed6548a4aa6ad4f7f6251865177dd70106b9
UA3MQJ <ua3mqj@gmail.com>
2019-08-07 23:58:40 +0300

    dirty_set perf_test add files

---
+++ test/perf5_test.exs
@@ -0,0 +1,42 @@
+defmodule Perf5Test do
+    use ExUnit.Case, async: false
+    doctest Barray
+    require Logger
+    import Gnuplot
+  
+    @tag perf5_test: true
+    # mix test --only perf5_test
+
+    test "perf dirty_write test 1Mb" do
+  
+      arr = Barray.new(1, 1024*1024*1024)
+
+      create_times = for x <- 1..1000 do
+
+        xx = (x * 1) * 1024 * 1024
+  
+        t1=:erlang.monotonic_time(:nanosecond)
+        :ok = Barray.dirty_set(arr, <<0>>, xx)
+        t2=:erlang.monotonic_time(:nanosecond)
+  
+        [x, (t2-t1) / 1]
+      end
+  
+      {:ok, _cmd} = plot([
+        [:set, :term, :pngcairo],
+        [:set, :output, "./dirty_set.png"],
+        [:set, :title, "Set element time of A(1Mb) each element equ 1 byte"],
+        [:set, :xlabel, "n"],
+        [:set, :ylabel, "Time (ns)"],
+        [:set, :key, :left, :top],
+        plots([
+            ["-", :title, "get time", :with, :line]
+        ])
+        ],
+        [
+          create_times
+        ])
+    end
+  
+end
+  
