354d3b34d3f93d798193754f8a38032439f82d84
UA3MQJ <ua3mqj@gmail.com>
2019-08-20 16:11:15 +0300

    v1.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Barray.MixProject do
   def project do
     [
       app: :barray,
-      version: "0.1.0",
+      version: "1.0.1",
       elixir: "~> 1.6",
       compilers: [:elixir_make] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
